23004e40548fde08c49fcc1d4f4c851aeea0606f
Flaviu Emanuel Hongu <fhongu@c2r15s6.42madrid.com>
2023-09-27 20:43:17 +0200

    New info

--- monitoring.sh
+++ monitoring.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Memory variables
+USEDMEM=$(free --mega | awk '$1=="Mem" {print $3}')
+TOTMEM=$(free --mega | awk '$1=="Mem" {print $2}')
+PERCMEM=$(free --mega | awk '$1=="Mem" {printf("(%.2f%%)\n", $3/$2*100)}')
+# Disk variables
+USEDDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} END {print used_disk}')
+TOTDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{total_disk += $2} END {printf ("%.0fGb\n"), total_disk/1024}')
+
+
 echo "#Architecture: $(uname -a)"
 
-echo "#CPU physical : $(cat /proc/cpuinfo | grep cores | awk 'NR==1' | cut --delimiter=' ' -f 3)"
+echo "#CPU physical : $(grep cores /proc/cpuinfo | awk 'NR==1' | cut --delimiter=' ' -f 3)"
+
+echo "#vCPU : $(grep processor /proc/cpuinfo | wc -l)"
+
+echo "#Memory Usage: $USEDMEM/$TOTMEM $PERCMEM"
+
+echo "#Disk Usage: $USEDDSK/$TOTDSK "
+
