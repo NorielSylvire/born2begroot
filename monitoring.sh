978c354ac497e15f48ca8f6bdcadc74657677607
Flaviu Emanuel Hongu <fhongu@c2r17s1.42madrid.com>
2023-09-29 23:27:28 +0200

    Switched to wall

--- monitoring.sh
+++ monitoring.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
 
+# Architecture
+ARCH=$(uname -a)
+
+# CPU
+PCPU=$(grep "physical id" /proc/cpuinfo | wc -l)
+VCPU=$(grep processor /proc/cpuinfo | wc -l)
+CPUL=$(vmstat 1 3 | tail -1 | awk '{printf ("%.1f%%"), 100-$15}')
+
 # Memory variables
-USEDMEM=$(free --mega | awk '$1=="Mem:" {print $3}')
+USDMEM=$(free --mega | awk '$1=="Mem:" {print $3}')
 TOTMEM=$(free --mega | awk '$1=="Mem:" {print $2}')
-PERCMEM=$(free --mega | awk '$1=="Mem:" {printf("(%.2f%%)\n", $3/$2*100)}')
+PRCMEM=$(free --mega | awk '$1=="Mem:" {printf("(%.2f%%)\n", $3/$2*100)}')
 # Disk variables
-USEDDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} END {print used_disk}')
+USDDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} END {print used_disk}')
 TOTDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{total_disk += $2} END {printf ("%.1fGb\n"), total_disk/1024}')
-PERCDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} {total_disk += $2} END {printf ("(%d%%)\n"), used_disk/total_disk*100}')
+PRCDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} {total_disk += $2} END {printf ("(%d%%)\n"), used_disk/total_disk*100}')
 
-# CPU variables
-CPULD=$(vmstat 1 3 | tail -1 | awk '{printf ("%.1f%%"), 100-$15}')
-
-echo "#Architecture: $(uname -a)"
-
-echo "#CPU physical : $(grep "physical id" /proc/cpuinfo | wc -l)"
-
-echo "#vCPU : $(grep processor /proc/cpuinfo | wc -l)"
-
-echo "#Memory Usage: $USEDMEM/${TOTMEM}MB $PERCMEM"
-
-echo "#Disk Usage: $USEDDSK/$TOTDSK $PERCDSK"
-
-echo "#CPU load: $CPULD"
-
-echo "#Last boot: $(who -b | awk '$1=="system" {print $3 " " $4}')"
+# Last boot
+LB=$(who -b | awk '$1=="system" {print $3 " " $4}')
 
 if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then
-  echo "#LVM use: yes"
+  LVMU="yes"
 else
-  echo "#LVM use: no"
+  LVMU="no"
 fi
 
-echo "#TCP Connections : $(ss -t -a | grep ESTAB | wc -l) ESTABLISHED"
 
-echo "#User log: $(users | wc -w)"
+# Network
+TCPC=$(ss -t -a | grep ESTAB | wc -l)
+IPV4=$(hostname -I)
+MACA=$(ip link | grep "link/ether" | awk '{print $2}')
 
-echo "#Network: IP $(hostname -I) ($(ip link | grep "link/ether" | awk '{print $2}'))"
+# Users
+USRS=$(users | wc -w)
 
-echo "#Sudo : $(journalctl _COMM=sudo | grep COMMAND | wc -l)"
+# Commands
+CMDS=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+
+wall "  #Architecture: $ARCH
+        #CPU physical : $PCPU
+        #vCPU : $VCPU
+        #Memory Usage: $USDMEM/${TOTMEM}MB $PRCMEM
+        #Disk Usage: $USDDSK/$TOTDSK $PRCDSK
+        #CPU load: $CPUL
+        #Last boot: $LB
+        #LVM use: $LVMU
+        #TCP Connections : $TCPC ESTABLISHED
+        #User log: $USRS
+        #Network: IP $IPV4 ($MACA)
+        #Sudo : $CMDS"
 
