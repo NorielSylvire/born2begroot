f396643c1b70e6a0c80e046c5750b674a03a848d
Flaviu Emanuel Hongu <fhongu@c2r17s1.42madrid.com>
2023-09-29 21:51:44 +0200

    dsk usage

--- monitoring.sh
+++ monitoring.sh
@@ -7,7 +7,7 @@ PERCMEM=$(free --mega | awk '$1=="Mem" {printf("(%.2f%%)\n", $3/$2*100)}')
 # Disk variables
 USEDDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} END {print used_disk}')
 TOTDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{total_disk += $2} END {printf ("%.0fGb\n"), total_disk/1024}')
-
+PERCDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} {total_disk += $2} END {printf ("%d%%\n"), used_disk/total_disk*100}')
 
 echo "#Architecture: $(uname -a)"
 
