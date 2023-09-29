ff0a14e4f2c6ea3367464601f939b48d768b7c4f
Flaviu Emanuel Hongu <fhongu@c2r17s1.42madrid.com>
2023-09-29 22:13:11 +0200

    CPULD

--- monitoring.sh
+++ monitoring.sh
@@ -7,7 +7,10 @@ PERCMEM=$(free --mega | awk '$1=="Mem:" {printf("(%.2f%%)\n", $3/$2*100)}')
 # Disk variables
 USEDDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} END {print used_disk}')
 TOTDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{total_disk += $2} END {printf ("%.0fGb\n"), total_disk/1024}')
-PERCDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} {total_disk += $2} END {printf ("%d%%\n"), used_disk/total_disk*100}')
+PERCDSK=$(df -m | grep "/dev" --exclude=/boot | awk '{used_disk += $3} {total_disk += $2} END {printf ("(%d%%)\n"), used_disk/total_disk*100}')
+
+# CPU variables
+CPULD=$(vmstat 1 4 | tail -1 | awk '{printf ("%f%%"), 100-$15}')
 
 echo "#Architecture: $(uname -a)"
 
@@ -18,4 +21,6 @@ echo "#vCPU : $(grep processor /proc/cpuinfo | wc -l)"
 echo "#Memory Usage: $USEDMEM/$TOTMEM $PERCMEM"
 
 echo "#Disk Usage: $USEDDSK/$TOTDSK $PERCDSK"
+
+echo "#CPU load: $CPULD"
 
